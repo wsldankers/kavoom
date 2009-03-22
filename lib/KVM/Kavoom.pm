99d9f44d425fe1d13f58d3946d5afc21f76ba5f8
wsl <wsl@03f5a900-3ce9-423d-a260-47407372f702>
2009-03-22 16:05:06 +0000

    git-svn-id: file:///dev/shm/out/svn/trunk@1565 03f5a900-3ce9-423d-a260-47407372f702

---
+++ lib/KVM/Kavoom.pm
@@ -0,0 +1,202 @@
+use strict;
+use warnings FATAL => 'all';
+use utf8;
+
+package KVM::Kavoom;
+
+use Spiffy -Base;
+use IO::Socket::UNIX;
+use Expect;
+
+our $configdir = '/etc/kavoom';
+our $statedir = '/var/lib/kavoom';
+our $rundir = '/var/run/kavoom';
+
+field 'name';
+field 'id';
+field 'disks' => [];
+field 'nics' => [];
+field 'args';
+
+sub new {
+	my $name = $_[0];
+	die unless defined $name;
+
+	my $id;
+	if(open ID, '<', "$statedir/$name.id") {
+		my $line = <ID>;
+		chomp $line;
+		$id = int($line);
+		close ID or die;
+	} else {
+		my $seq;
+		if(open SEQ, '<', "$statedir/.seq") {
+			my $line = <SEQ>;
+			chomp $line;
+			$seq = int($line);
+			close SEQ or die;
+		} else {
+			$seq = 0;
+		}
+		$id = $seq++;
+
+		open SEQ, '>', "$statedir/.seq,new"
+			or die "Can't open $statedir/.seq,new for writing: $!\n";
+		print SEQ "$seq\n" or die;
+		close SEQ or die;
+
+		open ID, '>', "$statedir/$name.id,new"
+			or die "Can't open $statedir/$name.id,new for writing: $!\n";
+		print ID "$id\n" or die;
+		close ID or die;
+
+		rename "$statedir/.seq,new", "$statedir/.seq"
+			or die "Can't rename $statedir/.seq,new to $statedir/.seq: $!\n";
+		rename "$statedir/$name.id,new", "$statedir/$name.id"
+			or die "Can't rename $statedir/$name.id,new to $statedir/$name.id: $!\n";
+	}
+	die unless defined $id;
+
+	my $args = {
+		name => $name,
+		vnc => 'none',
+		usbdevice => 'tablet',
+#		monitor => 'tcp:localhost:'.(4000+$id).',server,nowait,nodelay',
+		serial => "unix:$rundir/$name.serial,server,nowait",
+		monitor => "unix:$rundir/$name.monitor,server,nowait",
+		pidfile => "$rundir/$name.pid"
+	};
+
+	$self = super(name => $name, id => $id, args => $args);
+}
+
+sub trim() {
+	foreach(@_) {
+		s/(^\s+|\s+$)//g;
+		s/\s+/ /;
+	}
+}
+
+sub bool() {
+	local $_ = $_[0];
+	return 1 if /^([yjt]|(on|1)$)/;
+	return 0 if /^([nf]|(off|0)$)/;
+	return $_[1];
+}
+
+our %keys; @keys{qw(mem cpus mac vnc boot disk drive)} = ();
+
+sub mem {
+	my $args = $self->args;
+	$args->{m} = int($_[0])
+}
+
+sub cpus {
+	my $args = $self->args;
+	$args->{smp} = int($_[0])
+}
+
+sub mac {
+	my $nics = $self->nics;
+	push @$nics, $_[0]
+}
+
+sub vnc {
+	my $id = $self->id;
+	my $args = $self->args;
+	$args->{vnc} = bool($_[0]) ? ":$id" : 'none'
+}
+
+sub disk {
+	my $disks = $self->disks;
+	push @$disks, "media=disk,file=$_[0],cache=off"
+}
+
+sub drive {
+	my $disks = $self->disks;
+	push @$disks, $_[0]
+}
+
+sub config {
+	my $name = $self->name;
+	my $cfg = @_ ? $_[0] : "$configdir/$name.cfg";
+	open CFG, '<', $cfg
+		or die "Can't open $cfg: $!\n";
+
+	while(<CFG>) {
+		next if /^\s*($|#)/;
+		my ($key, $val) = split('=', $_, 2);
+		die "Malformed line at $cfg:$.\n"
+			unless defined $val;
+		trim($key, $val);
+		my $lkey = lc $key;
+		die "unknown configuration parameter at $cfg:$.: $key\n"
+			unless exists $keys{$lkey};
+		$self->$lkey($val);
+	}
+
+	close CFG or die;
+}
+
+sub command {
+	my @cmd = qw(kvm -daemonize);
+
+	my $name = $self->name;
+	my $id = $self->id;
+
+	my $args = $self->args;
+	while(my ($key, $val) = each(%$args)) {
+		push @cmd, "-$key", $val;
+	}
+
+	my $nics = $self->nics;
+	my $i = 0;
+	foreach my $mac (@$nics) {
+		push @cmd,
+			'-net', "tap,vlan=$i,ifname=$name$i",
+#			'-net', "nic,vlan=$i,macaddr=$mac";
+			'-net', "nic,vlan=$i,model=e1000,macaddr=$mac";
+#			'-net', "nic,vlan=$i,model=virtio,macaddr=$mac";
+		$i++;
+	}
+	push @cmd, '-net', 'none'
+		unless $i;
+
+	my $disks = $self->disks;
+	foreach my $disk (@$disks) {
+		push @cmd, '-drive', $disk;
+	}
+
+	return \@cmd;
+}
+
+sub sh {
+	my $cmd = $self->command(@_);
+	return join(' ', map { s|[^A-Z0-9_.,=:+/-]|\\$&|gi; $_ } @$cmd)
+}
+
+sub socket {
+	my $name = $self->name;
+	my $type = shift;
+	my $sock = new IO::Socket::UNIX(
+		Peer => "$rundir/$name.$type",
+		Type => SOCK_STREAM,
+		Timeout => 1,
+		@_
+	);
+	unless(defined $sock) {
+		return undef if $!{ECONNREFUSED};
+		die "can't create socket: $!\n";
+	}
+	my $exp = Expect->exp_init($sock);
+	die "can't create Expect object: $!\n" unless defined $exp;
+	return $exp;
+}
+
+sub monitor {
+	return $self->socket('monitor')
+}
+
+sub serial {
+	return $self->socket('serial')
+}
